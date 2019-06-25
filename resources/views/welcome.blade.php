05852ad77423d59575426d3a4cbdf1f95637d288
zhengyuanhong <1713639570@qq.com>
2019-06-25 17:05:02 +0800

    fix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -86,7 +86,7 @@
 
                 <div class="links">
                     <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
+                    <a href="https://laracasts.com">delete</a>
                     <a href="https://laravel-news.com">News</a>
                     <a href="https://blog.laravel.com">Blog</a>
                     <a href="https://nova.laravel.com">Nova</a>
